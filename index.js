df8de6e34984bff4750c14d59e8bd13ec345520a
Kbuczynski <kamil.buczynski00@interia.pl>
2021-06-15 12:03:07 +0200

    create websocket connection

---
+++ index.js
@@ -0,0 +1,68 @@
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+const webSocketsServerPort = 8000;
+
+const server = http.createServer((req, res) => {
+    res.write("Hello world!");
+    res.end();
+}).listen(webSocketsServerPort);
+
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+const clients = {};
+let gates = '[]';
+
+const broadcast = (clients, message, currentClient) => {
+    console.log(Object.keys(clients))
+
+    for(const key of Object.keys(clients)){
+        if(key === currentClient) return;
+        clients[key].sendUTF(message.utf8Data)
+    }
+
+    // if (Object.keys(clients) === currentClient)
+    // for (let i = 0; i < clients.length; i++) {
+    //     console.log(clients[i])
+    //     if (clients[currentClient]) return;
+    //
+    //
+    //
+    //     clients[i].sendUTF(message.utf8Data);
+    // }
+}
+
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', (request) => {
+    const userID = getUniqueID();
+    console.log((new Date()) + ' Received a new connection from origin ' + request.origin + '.');
+
+    const connection = request.accept(null, request.origin);
+    console.log(request.origin);
+    clients[userID] = connection;
+
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+
+    connection.sendUTF(gates);
+
+    connection.on('message', (message) => {
+        if (message.type === 'utf8') {
+            if (gates !== message) {
+                gates = message;
+                console.log('Received Message: ' + message.utf8Data);
+                broadcast(clients, message, userID);
+            }
+        }
+    })
+
+    connection.on('close', () => {
+        delete clients[userID];
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
